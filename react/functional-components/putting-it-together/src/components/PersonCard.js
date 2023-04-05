30b49ef79df2f856db59aa531752c14805c890f1
Alexander Vice <alexdvice@gmail.com>
2023-04-05 11:08:28 -0700

    completed putting-it-together

--- react/functional-components/putting-it-together/src/components/PersonCard.js
+++ react/functional-components/putting-it-together/src/components/PersonCard.js
@@ -1,15 +1,19 @@
-import React from 'react';
-
-
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age);
+
+    const birthdayClick = () => {
+        setAge(+age+1)
+    };
     return (
         <div className='PersonCard'>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={birthdayClick}> Birthday Button for {props.firstName} {props.lastName} </button>
         </div>
-    )
+    );
 }
 
 export default PersonCard;
