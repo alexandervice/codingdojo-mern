1f9e4217a94e8bd33cd1cb2d5a14bd4050e2c3cc
Alexander Vice <alexdvice@gmail.com>
2023-04-05 10:50:59 -0700

    copied prop-it up into putting-it-together

--- react/functional-components/putting-it-together/src/App.js
+++ react/functional-components/putting-it-together/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName={'Alexander'} lastName = {'Vice'} age={'29'} hairColor={'Blonde'}/>
+      <PersonCard firstName={'Clark'} lastName = {'Kent'} age={'34'} hairColor={'Brown'}/>
+      <PersonCard firstName={'Bruce'} lastName = {'Wayne'} age={'33'} hairColor={'Black'}/>
+      <PersonCard firstName={'Diana'} lastName = {'Prince'} age={'32'} hairColor={'Black'}/>
     </div>
   );
 }
