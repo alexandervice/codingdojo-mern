57cf3cd52b03c0ef07755691ab633dd85fc54a27
Alexander Vice <alexdvice@gmail.com>
2023-04-05 15:43:59 -0700

    updated css

--- react/functional-components/hook-form/src/components/UserForm.js
+++ react/functional-components/hook-form/src/components/UserForm.js
@@ -19,35 +19,36 @@ const UserForm = () => {
 
     };
     return (
-        <div>
+        <div className='userClass'>
             <form className='createUser' onSubmit={ createUser }>
                 <div>
                     <label>First Name:</label>
-                    <input type='text' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                    <input className='formInput' type='text' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                 </div>
                 <div>
                     <label>Last Name:</label>
-                    <input type='text' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    <input className='formInput' type='text' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                 </div>
                 <div>
                     <label>Email:</label>
-                    <input type='text' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input className='formInput' type='text' value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div>
                     <label>Password:</label>
-                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input className='formInput' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <label>Confirm Password:</label>
-                    <input type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                    <input className='formInput' type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </div>
             </form>
+            <h4>Your Form Data:</h4>
             <div className='PersonCard'>
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-                <p>Confirm Password: {confirmPassword}</p>
+                <p><b>First Name:</b> {firstName}</p>
+                <p><b>Last Name:</b> {lastName}</p>
+                <p><b>Email:</b> {email}</p>
+                <p><b>Password:</b> {password}</p>
+                <p><b>Confirm Password:</b> {confirmPassword}</p>
             </div>
         </div>
     );
