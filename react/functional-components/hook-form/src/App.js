46f4f8f4475845abeace056b283d45e554aa911b
Alexander Vice <alexdvice@gmail.com>
2023-04-05 12:57:23 -0700

    created hook-form

---
+++ react/functional-components/hook-form/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PersonCard from './components/PersonCard'
+
+const App = () => {
+  return (
+    <div className="App">
+      <PersonCard firstName={'Alexander'} lastName = {'Vice'} age={'29'} hairColor={'Blonde'}/>
+      <PersonCard firstName={'Clark'} lastName = {'Kent'} age={'34'} hairColor={'Brown'}/>
+      <PersonCard firstName={'Bruce'} lastName = {'Wayne'} age={'33'} hairColor={'Black'}/>
+      <PersonCard firstName={'Diana'} lastName = {'Prince'} age={'32'} hairColor={'Black'}/>
+    </div>
+  );
+}
+
+export default App;
