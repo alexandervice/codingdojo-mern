c643b323479d4fa608fe18271c7e711c1bad7e1d
Alexander Vice <alexdvice@gmail.com>
2023-04-15 15:27:32 -0700

    updated project

---
+++ react/functional-components/box-generator/src/components/CreateBox.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import styles from "./BoxStyles.module.css"
+
+
+
+const CreateBox = () => {
+  return (
+    <div className={styles.box}></div>
+  )
+}
+
+export default CreateBox
---
+++ react/functional-components/box-generator/src/components/DeclareColor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeclareColor = () => {
+  return (
+    <div>DeclareColor</div>
+  )
+}
+
+export default DeclareColor
