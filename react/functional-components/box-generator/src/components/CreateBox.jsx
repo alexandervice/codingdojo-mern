3de3119d9291c3f470f3a33333f3c9618e3cbd11
Alexander Vice <alexdvice@gmail.com>
2023-04-15 15:48:16 -0700

    updated app

--- react/functional-components/box-generator/src/components/CreateBox.jsx
+++ react/functional-components/box-generator/src/components/CreateBox.jsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react'
-import styles from "./BoxStyles.module.css"
 
 
 
-const CreateBox = () => {
-  return (
-    <div className={styles.box}></div>
-  )
+const CreateBox = (props) => {
+    const boxStyle = {
+        height: "100px",
+        width: "100px",
+        color: {props.color}
+    }
+    const makeBox = () => {
+
+    }
+    return (
+        <div className= {boxStyle} ></div>
+    )
 }
 
 export default CreateBox
--- react/functional-components/box-generator/src/components/DeclareColor.jsx
+++ react/functional-components/box-generator/src/components/DeclareColor.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const DeclareColor = () => {
-  return (
-    <div>DeclareColor</div>
-  )
+const DeclareColor = (props) => {
+    const [color, setColor] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onNewColor(color)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='color'>Set Color</label>
+                <input type="color" name="color"/>
+                <input type='' value="Add Box"  />
+            </form>
+        </div>
+    )
 }
 
 export default DeclareColor
