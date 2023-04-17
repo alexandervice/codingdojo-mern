9220f16ceba8085463e01d89520f025eb5463333
Alexander Vice <alexdvice@gmail.com>
2023-04-17 13:58:42 -0700

    saved to local state

--- react/functional-components/todo-list/src/App.js
+++ react/functional-components/todo-list/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MakeList from './components/MakeList';
 import RenderTodoList from './components/RenderTodoList';
 
 function App() {
-  const [todoArray, setTodoArray] = useState([])
+  const [todoArray, setTodoArray] = useState(() => {
+    const savedState = localStorage.getItem("stateString");
+    const todoArray = JSON.parse(savedState);
+    return todoArray || [];
+  });
+  useEffect(() => {
+    localStorage.setItem("stateString", JSON.stringify(todoArray));
+  }, [todoArray]);
   return (
     <div className="App">
       <MakeList todoArray = {todoArray} setTodoArray = {setTodoArray}/>
