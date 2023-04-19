8548cbc794529a988197e5a3e721cd58186dc42f
Alexander Vice <alexdvice@gmail.com>
2023-04-19 15:00:03 -0700

    created the tabs assignment
    practice assignment

---
+++ react/functional-components/tabs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, {useState} from 'react';
+import Tabs from './components/Tabs';
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
