e7fa20be6547eaa4e880cc653b08b4181b74175c
Alexander Vice <alexdvice@gmail.com>
2023-04-20 09:56:37 -0700

    creating tab components

--- react/functional-components/tabs/src/App.js
+++ react/functional-components/tabs/src/App.js
@@ -4,10 +4,15 @@ import Tabs from './components/Tabs';
 
 function App() {
   const[tabArray, setTabArray] = useState([]);
-  setTabArray(["Tab content 1", "Tab content 2", "Tab content 3"])
+  setTabArray([
+    {label: "Tab 1", content: "Tab 1 content"},
+    {label: "Tab 2", content: "Tab 2 content"},
+    {label: "Tab 3", content: "Tab 3 content"}
+  ])
+  const[tabIndex, setTabIndex] = useState(1)
   return (
     <div className="App">
-      <Tabs tabArray = {tabArray}/>
+      <Tabs tabArray = {tabArray} tabIndex={tabIndex} setTabIndex={setTabIndex}/>
     </div>
   );
 }
--- react/functional-components/tabs/src/components/Tabs.jsx
+++ react/functional-components/tabs/src/components/Tabs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Tabs = ({tabArray}) => {
+const Tabs = ({tabArray, tabIndex, setTabIndex}) => {
+  
+  const updateTabIndex = (e) => {
+    e.preventDefault();
+    setTabIndex(e.target.value);
+  }
   return (
-    <div>Tabs</div>
+    <div>
+      {tabArray.map((list, index) => (
+        <div key={index} className='tab'>
+          <button className='tabLabel' value={index+1} onClick={updateTabIndex}>{list.label}</button>
+        </div>
+      ))
+      }
+      <div className="tabContent">
+        {tabArray.content[tabIndex]}
+      </div>
+    </div>
   )
 }
 
