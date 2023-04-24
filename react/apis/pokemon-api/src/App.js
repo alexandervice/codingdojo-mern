4a87bc97f9ae3ab344da629336e530c77b601a75
Alexander Vice <alexdvice@gmail.com>
2023-04-24 14:00:53 -0700

    completed app

--- react/apis/pokemon-api/src/App.js
+++ react/apis/pokemon-api/src/App.js
@@ -5,14 +5,15 @@ function App() {
   const [pokemonList, setPokemonList] = useState([]);
   
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1010")
       .then(response => {
         // not the actual JSON response body but the entire HTTP response
         return response.json();
       })
       .then(response => {
         // we now run another promise to parse the HTTP response into usable JSON
-        console.log(response);
+        // console.log(response.results);
+        setPokemonList(response.results)
         // setPokemonList(response)
       })
       .catch(err=>{
@@ -22,7 +23,13 @@ function App() {
 
   return (
     <div className="App">
-      
+      {
+        pokemonList.map((list, index) => {
+          return(
+            <p key={index}>{list.name}</p>
+          )
+        })
+      }
     </div>
   );
 }
