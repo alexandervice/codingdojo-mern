53ad7e91eda2b21faf0c9b65ff949abb8b80dfe7
Alexander Vice <alexdvice@gmail.com>
2023-04-24 13:08:17 -0700

    Update App.js

--- react/apis/pokemon-api/src/App.js
+++ react/apis/pokemon-api/src/App.js
@@ -2,6 +2,21 @@ import React, {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(response => {
+        // not the actual JSON response body but the entire HTTP response
+        return response.json();
+      }).then(response => {
+        // we now run another promise to parse the HTTP response into usable JSON
+        console.log(response);
+      }).catch(err=>{
+        console.log(err);
+      });
+  }, [pokemonList]);
+
   return (
     <div className="App">
       
