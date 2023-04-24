c8f932695176ba9f20577aa98a08160966e8c507
Alexander Vice <alexdvice@gmail.com>
2023-04-24 13:48:42 -0700

    Update App.js

--- react/apis/pokemon-api/src/App.js
+++ react/apis/pokemon-api/src/App.js
@@ -9,13 +9,16 @@ function App() {
       .then(response => {
         // not the actual JSON response body but the entire HTTP response
         return response.json();
-      }).then(response => {
+      })
+      .then(response => {
         // we now run another promise to parse the HTTP response into usable JSON
         console.log(response);
-      }).catch(err=>{
+        // setPokemonList(response)
+      })
+      .catch(err=>{
         console.log(err);
       });
-  }, [pokemonList]);
+  }, []);
 
   return (
     <div className="App">
