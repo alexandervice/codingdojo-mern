d34cc9031965463e643d5022593c83a3d5cdf9ab
Alexander Vice <alexdvice@gmail.com>
2023-04-25 12:58:09 -0700

    updated project

---
+++ react/react-routing/routing-practice/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ react/react-routing/routing-practice/src/components/OtherRoutes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+
+const OtherRoutes = () => {
+  return (
+    <div>OtherRoutes</div>
+  )
+}
+
+export default OtherRoutes
