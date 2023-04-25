a149e3533851cd7b639d28d02e5370e7774bed89
Alexander Vice <alexdvice@gmail.com>
2023-04-25 15:06:48 -0700

    Update Planets.jsx

--- react/react-routing/luke-apiwalker/src/components/Planets.jsx
+++ react/react-routing/luke-apiwalker/src/components/Planets.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import {useParams} from "react-router-dom"
 
 const Planets = () => {
+  const [planet, setPlanet] = useState({});
+
+  const{index} = useParams();
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/${index}`)
+      .then(response => {
+        // console.log(response.data.result)
+        setPlanet(response.data.results)
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>Planets</div>
+    <div>
+      <h2>{planet.name}</h2>
+      
+    </div>
   )
 }
 
