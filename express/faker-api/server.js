1aabf0d1f6e797f96e5a6ece9abd2aeac61cf24e
Alexander Vice <alexdvice@gmail.com>
2023-04-27 13:09:09 -0700

    project complete

--- express/faker-api/server.js
+++ express/faker-api/server.js
@@ -11,16 +11,42 @@ const createUser = () => {
     lastName: faker.name.lastName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-    phoneNumber: faker.phone.number(),
+    phoneNumber: faker.phone.number()
   };
   return newFakeUser;
 };
+
+const createCompany = () => {
+  const newFakeCompany = {
+    _id: faker.finance.account(5),
+    name: faker.company.name(),
+    address: {
+      street: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipCode: faker.address.zipCode(),
+      country: faker.address.country()
+    }
+  };
+  return newFakeCompany;
+};
     
+const newCompany = createCompany();
+console.log(newCompany);
+
 const newUser = createUser();
 console.log(newUser);
 
-app.get("/api", (req, res) => {
-  res.json(newUser);
+app.get("/api/users/new", (req, res) => {
+  res.json( newUser );
+});
+
+app.get("/api/companies/new", (req, res) => {
+  res.json( newCompany );
+});
+
+app.get("/api/user/company", (req, res) => {
+  res.json( [newUser, newCompany] );
 });
 
 // this needs to be below the other code blocks
