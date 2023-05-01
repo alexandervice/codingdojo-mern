52c55c222615c7affb8e1bb6688378a0f3058f55
Alexander Vice <alexdvice@gmail.com>
2023-05-01 14:08:05 -0700

    Update server.js

--- mongoDB/jokes-api/server/server.js
+++ mongoDB/jokes-api/server/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
+    
+require("./config/mongoose.config");
+    
+app.use(express.json(), express.urlencoded({ extended: true }));
+    
+const AllJokeRoutes = require("./routes/jokes.route");
+AllJokeRoutes(app);
+    
+app.listen(8000, () => console.log("The server is all fired up on port 8000"));
 
