9372e19c139ff12f8ed409a59cbd6a9351310e45
Alexander Vice <alexdvice@gmail.com>
2023-05-02 12:17:15 -0700

    Update ProductForm.jsx

---
+++ full-stack-mern/product-manager-part1/client/src/components/ProductForm.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const ProductForm= () => {
+  const [product, setProduct] = useState({
+    name: "",
+    price: 0,
+    description: ""
+  });
+  // const [ name, setName ] = useState("")
+  // const [ price, setPrice ] = useState(0)
+  // const [ description, setDescription ] = useState("")
+  useEffect(()=>{
+      axios.get("http://localhost:8000/api")
+          .then(res=>setProduct(res.data.product))
+          .catch(err=>console.log(err))
+  }, []);
+  return (
+      <div>
+          <h2>Name: {product.name}</h2>
+          <h2>Price: {product.price}</h2>
+          <h2>Description: {product.description}</h2>
+      </div>
+  )
+}
+export default ProductForm;
+
