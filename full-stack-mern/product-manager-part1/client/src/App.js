6c62f4a0d6d52c3e4d8b5af8babf1be6c9722b00
Alexander Vice <alexdvice@gmail.com>
2023-05-02 13:47:54 -0700

    Update App.js

--- full-stack-mern/product-manager-part1/client/src/App.js
+++ full-stack-mern/product-manager-part1/client/src/App.js
@@ -4,6 +4,7 @@ import ProductForm from './components/ProductForm';
 function App() {
   return (
     <div className="App">
+      <h1>Create New Product</h1>
       <ProductForm/>
     </div>
   );
