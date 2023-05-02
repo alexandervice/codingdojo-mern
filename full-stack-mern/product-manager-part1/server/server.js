6bc8f5d61456e58c449223671e9b765f04eff65d
Alexander Vice <alexdvice@gmail.com>
2023-05-02 11:21:02 -0700

    Update server.js

--- full-stack-mern/product-manager-part1/server/server.js
+++ full-stack-mern/product-manager-part1/server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+require("./config/mongoose.config");
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+const AllProductRoutes = require("./routes/products.route");
+AllProductRoutes(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
