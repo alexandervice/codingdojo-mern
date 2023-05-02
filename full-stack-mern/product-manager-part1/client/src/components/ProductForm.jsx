5b9c54dd299e2ca4489b90af1845e5c5fcf95cc1
Alexander Vice <alexdvice@gmail.com>
2023-05-02 13:41:14 -0700

    Update ProductForm.jsx

--- full-stack-mern/product-manager-part1/client/src/components/ProductForm.jsx
+++ full-stack-mern/product-manager-part1/client/src/components/ProductForm.jsx
@@ -1,24 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 const ProductForm= () => {
-  const [product, setProduct] = useState({
-    name: "",
-    price: 0,
-    description: ""
-  });
-  // const [ name, setName ] = useState("")
-  // const [ price, setPrice ] = useState(0)
-  // const [ description, setDescription ] = useState("")
-  useEffect(()=>{
-    axios.post("http://localhost:8000/api/products/create")
-      .then(res=>setProduct(res.data.product))
-      .catch(err=>console.log(err))
-  }, []);
+  // const [product, setProduct] = useState({
+  //   name: "",
+  //   price: 0,
+  //   description: ""
+  // });
+  const [ name, setName ] = useState("")
+  const [ price, setPrice ] = useState(0)
+  const [ description, setDescription ] = useState("")
+  // useEffect(()=>{
+  //   axios.post("http://localhost:8000/api/products/create")
+  //     .then(res=>setProduct(res.data.product))
+  //     .catch(err=>console.log(err))
+  // }, []);
+
+  const onSubmitHandler = (e) => {
+  e.preventDefault();
+  //make a post request to create a new product
+  axios.post('http://localhost:8000/api/products/create', {
+    name,
+    price,
+    description
+  })
+    .then(res=>{
+      console.log(res);
+      console.log(res.data);
+    })
+    .catch(err=>console.log(err))
+}
+
   return (
     <div>
-      <h2>Name: {product.name}</h2>
-      <h2>Price: {product.price}</h2>
-      <h2>Description: {product.description}</h2>
+      <form onSubmit={onSubmitHandler}>
+        <h5>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name='name' onChange={(e) => setName(e.target.value)}/>
+        </h5>
+        <h5>
+          <label htmlFor="price">Price:</label>
+          <input type="number" name='price' onChange={(e) => setPrice(e.target.value)}/>
+        </h5>
+        <h5>
+          <label htmlFor="description">Description:</label>
+          <input type="text" name='description' onChange={(e) => setDescription(e.target.value)}/>
+        </h5>
+      </form>
     </div>
   )
 }
