055eeb757e6a366d0e1f6a8ba910b9549e72ef9b
Alexander Vice <alexdvice@gmail.com>
2023-05-02 14:41:39 -0700

    completed assignment

--- full-stack-mern/product-manager-part1/client/src/components/ProductForm.jsx
+++ full-stack-mern/product-manager-part1/client/src/components/ProductForm.jsx
@@ -6,9 +6,10 @@ const ProductForm= () => {
   //   price: 0,
   //   description: ""
   // });
-  const [ name, setName ] = useState("")
-  const [ price, setPrice ] = useState(0)
-  const [ description, setDescription ] = useState("")
+  const [ name, setName ] = useState("");
+  const [ price, setPrice ] = useState(0);
+  const [ description, setDescription ] = useState("");
+  const [submissionStatus, setSubmissionStatus] = useState(false);
   // useEffect(()=>{
   //   axios.post("http://localhost:8000/api/products/create")
   //     .then(res=>setProduct(res.data.product))
@@ -17,6 +18,7 @@ const ProductForm= () => {
 
   const onSubmitHandler = (e) => {
   e.preventDefault();
+  setSubmissionStatus(true);
   //make a post request to create a new product
   axios.post('http://localhost:8000/api/products/create', {
     name,
@@ -32,22 +34,27 @@ const ProductForm= () => {
 
   return (
     <div>
-      <h3>Create a New Product</h3>
-      <form onSubmit={onSubmitHandler}>
-        <h5>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name='name' onChange={(e) => setName(e.target.value)}/>
-        </h5>
-        <h5>
-          <label htmlFor="price">Price:</label>
-          <input type="number" name='price' onChange={(e) => setPrice(e.target.value)}/>
-        </h5>
-        <h5>
-          <label htmlFor="description">Description:</label>
-          <input type="text" name='description' onChange={(e) => setDescription(e.target.value)}/>
-        </h5>
-        <input type="submit"/>
-      </form>
+      {submissionStatus ?
+      <h3>Thank you for submitting the form</h3> :
+      <div className='productForm'>
+        <h3>Create a New Product</h3>
+        <form onSubmit={onSubmitHandler}>
+          <h5>
+            <label htmlFor="name">Name: </label>
+            <input type="text" name='name' onChange={(e) => setName(e.target.value)}/>
+          </h5>
+          <h5>
+            <label htmlFor="price">Price: </label>
+            <input type="number" name='price' onChange={(e) => setPrice(e.target.value)}/>
+          </h5>
+          <h5>
+            <label htmlFor="description">Description: </label>
+            <input type="text" name='description' onChange={(e) => setDescription(e.target.value)}/>
+          </h5>
+          <input type="submit"/>
+        </form>
+      </div>
+      }
     </div>
   )
 }
