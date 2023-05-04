c5581d914529c8a4fd2b5c43094c9f5d5ee22b49
Alexander Vice <alexdvice@gmail.com>
2023-05-04 13:41:01 -0700

    Update products.controller.js

--- full-stack-mern/product-manager-part2/server/controllers/products.controller.js
+++ full-stack-mern/product-manager-part2/server/controllers/products.controller.js
@@ -1,5 +1,22 @@
 const Product = require("../models/products.model");
 
+
+// alternatively it can be done this way, so you don't need to type the module.exports everytime:
+// module.exports = {
+//   // CREATE
+//   createNewProduct: (req, res) => {
+//     Product.create(req.body)
+//       .then(newProduct => res.json(newProduct))
+//       .catch(err => res.status(400).json(err));
+//   },
+//   // READ
+//   findAllProducts: (req, res) => {
+//     Product.find()
+//       .then(allProducts => res.json(allProducts))
+//       .catch(err => res.json(err));
+//   }
+// }
+
 module.exports.findAllProducts = (req, res) => {
   Product.find()
     .then((allProducts) => {
@@ -49,3 +66,4 @@ module.exports.deleteProduct = (req, res) => {
     .catch((err) => {
       res.json({ message: 'Something went wrong', error: err })
     });}
+
