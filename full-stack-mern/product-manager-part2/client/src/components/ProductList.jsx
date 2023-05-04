f6f2a74130c7efd12d02840b8ad3bf93bcedae18
Alexander Vice <alexdvice@gmail.com>
2023-05-04 10:32:28 -0700

    Update ProductList.jsx

---
+++ full-stack-mern/product-manager-part2/client/src/components/ProductList.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react'
+import axios from 'axios';
+const PersonList = (props) => {
+  const {product, setProduct} = props;
+  
+  useEffect(()=>{
+    axios.get("http://localhost:8000/api/products/find/all")
+      .then((res)=>{
+        console.log(res.data);
+        setProduct(res.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  }, [])
+  
+  return (
+    <div>
+      {
+        product.map((product, index)=>{
+        return <p key={index}>{product.name}</p>
+        })
+      }
+    </div>
+  )
+}
+export default PersonList;
+
