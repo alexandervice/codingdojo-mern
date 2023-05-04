f29c3c63cbd6c3eccece8f27c24859405c9938ba
Alexander Vice <alexdvice@gmail.com>
2023-05-04 12:48:21 -0700

    Update App.js

--- full-stack-mern/product-manager-part2/client/src/App.js
+++ full-stack-mern/product-manager-part2/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Main from "../views/Main"
+import OneProduct from "../components/OneProduct"
 // import ProductForm from './components/ProductForm';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           {/* this is the default path */}
           <Route element={<Main/>} path="/home" default/>
+          <Route element={<OneProduct/>} path="/product/:id"/>
         </Routes>
       </BrowserRouter>
     </div>
