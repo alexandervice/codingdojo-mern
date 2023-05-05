e3960434a893e3f678175abe53039e77565a3ed7
Alexander Vice <alexdvice@gmail.com>
2023-05-04 18:14:50 -0700

    Update products.route.js

--- full-stack-mern/product-manager-part2/server/routes/products.route.js
+++ full-stack-mern/product-manager-part2/server/routes/products.route.js
@@ -3,7 +3,8 @@ const ProductController = require("../controllers/products.controller");
 module.exports = app => {
     app.get('/api/products/find/all', ProductController.findAllProducts);
     app.get('/api/products/find/:id', ProductController.findOneProduct);
-    app.patch('/api/products/update/:id', ProductController.updateProduct);
+    // Patch only changes values in the req.body
+    app.patch('/api/products/update/:id', ProductController.updateProduct); 
     app.post('/api/products/create', ProductController.createNewProduct);
     app.delete('/api/products/delete/:id', ProductController.deleteProduct);
 }
