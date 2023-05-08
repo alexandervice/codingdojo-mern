44f576e0a31598afe8449630a7396a62eb5d1da2
Alexander Vice <alexdvice@gmail.com>
2023-05-08 10:09:14 -0700

    Update Main.jsx

--- full-stack-mern/product-manager-part2/client/src/views/Main.jsx
+++ full-stack-mern/product-manager-part2/client/src/views/Main.jsx
@@ -3,14 +3,16 @@ import React, { useState } from 'react'
 import ProductForm from '../components/ProductForm';
 import ProductList from '../components/ProductList';
 const Main = (props) => {
-  
   const [product, setProduct] = useState([]);
+  const removeFromDom = id => {
+    setProduct(product.filter(product => product.id !== id))
+  }
   
   return (
     <div>
       <ProductForm product={product} setProduct={setProduct}/>
         <hr/>
-      <ProductList product={product} setProduct={setProduct}/>
+      <ProductList product={product} setProduct={setProduct} removeFromDom={removeFromDom}/>
     </div>
   )
 }
