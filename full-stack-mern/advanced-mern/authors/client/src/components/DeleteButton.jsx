66099a64a4fbe03b100eb283c5f894cd5406a4c3
Alexander Vice <alexdvice@gmail.com>
2023-05-10 11:01:49 -0700

    Update DeleteButton.jsx

--- full-stack-mern/advanced-mern/authors/client/src/components/DeleteButton.jsx
+++ full-stack-mern/advanced-mern/authors/client/src/components/DeleteButton.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import axios from 'axios';
 const DeleteButton = (props) => {
-  const { productId, successCallback } = props;
-  const deleteProduct = e => {
-    axios.delete(`http://localhost:8000/api/products/delete/${productId}`)
+  const { authorId, successCallback } = props;
+  const deleteAuthor = e => {
+    axios.delete(`http://localhost:8000/api/products/delete/${authorId}`)
       .then(res=>{
         successCallback();
       })
       .catch(err=>console.log(err))
   }
   return (
-    <button className='productItem' onClick={deleteProduct}>
+    <button className='productItem' onClick={deleteAuthor}>
       Delete
     </button>
   )
